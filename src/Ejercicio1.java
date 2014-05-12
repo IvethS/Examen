d06708fab6e0bf3e4a8b9aa5a684876b7217c925
IvethS <ivethsabillon@hotmail.com>
2014-05-12 08:26:13 -0600

    Update Ejercicio1.java

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -6,6 +6,9 @@ public class Ejercicio1 {
 		Scanner s= new Scanner(System.in);
 		System.out.print("Ingrese un rating de 1 a 5 para The Amazing SpiderMan2: ");
 		int num = s.nextInt();
+		//Este Case se realizo para que pudiera comparar el codigo ingresado si se fija lee la variable numero
+		// y en base a la variable numero se dan los casos, como puede observar case 1 muestra la palabra pesima
+		// de esa forma sucesivamente se va dando.
 		switch (num)
 		{
 		case 1 :
@@ -31,7 +34,7 @@ public class Ejercicio1 {
 			System.out.print("Califacion no Reconocida");
 			break;
 		}
-
+// aqui en lo ultimo es por si ingrese un numero que no esta dentro del rango no lo va reconocer.
 	}
 
 }
