61c367e298381b498152f5dc39a28e6ed8eed814
IvethS <ivethsabillon@hotmail.com>
2014-05-12 08:29:42 -0600

    Update Ejercicio3.java

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -5,6 +5,9 @@ public class Ejercicio3 {
 	public static void main(String[] args) {
 		//Codigo usado con lo aprendido en clase
 		Scanner s = new Scanner(System.in);
+		// en este caso lo que se realizo fue primero hacer lectura de todos los numeros
+		// luego declarar una variable para que pudiera hacer el calculo del promedio una vez hecho esto
+		// lee la variable promedio y muestra los datos en base a su resultado
 		System.out.print("Ingrese numero 1: ");
 		int num1 = s.nextInt();
 		System.out.print("Ingrese numero 2: ");
